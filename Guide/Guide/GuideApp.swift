2289a1e90c0622a5d7f19f08efee09a258eb2ac3
FullMetalFist <m.vilabrera@gmail.com>
2023-02-17 14:48:14 -0500

    initial commit

---
+++ Guide/Guide/GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideApp.swift
+//  Guide
+//
+//  Created by Michael Vilabrera on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
